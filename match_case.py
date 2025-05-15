e8696bc38b509a532f5171e707cf73eff5cec26e
KonstantinPristavskiy <konstantin.pristavskiy@gmail.com>
2025-05-15 17:27:31 +0200

    new file:   for.py 	new file:   match_case.py

---
+++ match_case.py
@@ -0,0 +1,12 @@
+pets = ["dog", "fish", "cat"]
+
+match pets:
+    case ["dog", "cat", _]:
+        # Випадок, коли є і собака, і кіт
+        print("There's a dog and a cat.")
+    case ["dog", _, _]:
+        # Випадок, коли є тільки собака
+        print("There's a dog.")
+    case _:
+        # Випадок для інших комбінацій
+        print("No dogs.")
